843d87ffd02788e98b280315f8eba3503e56244c
fireflyy85 <fireflyy85@gmail.com>
2017-06-22 18:17:41 -0500

    Add files via upload

---
+++ Number Reversal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int numb = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            while (numb > 0)
+            {
+                int rem = numb % 10;
+                reverse = (reverse * 10) + rem;
+                numb = numb / 10;
+                // I think im going to enjoy working with C#
+                // Google has been my friend
+                // Google cares about me
+                // I hope these comments made you smile
+            }
+            Console.WriteLine("Reverse number={0}", reverse);
+            Console.ReadLine();
+
+
+        }
+    }
+}
